9352c51daff9b6bc51390b162e09dc860dcc2154
Yevgeny Pats <yp@fuzzit.dev>
2019-11-01 23:39:22 +0200

    bump to 1.0.2 and fix setup.py requirements

--- setup.py
+++ setup.py
@@ -3,19 +3,20 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setuptools.setup(
     name="pythonfuzz",
-    version="1.0.1",
+    version="1.0.2",
     author="fuzzit.dev",
     author_email="support@fuzzit.dev",
     description="coverage-guided fuzz testing for python",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/fuzzitdev/pythonfuzz",
-    install_requires=requirements,
+    install_requires=[
+        'coverage==4.5.4',
+        'psutil==5.6.3',
+        'numpy==1.17.3',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",

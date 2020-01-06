d685d2de0be43c5b1745807be4350a371e4421ca
Charles Ferguson <gerph@gerph.org>
2020-01-06 23:44:40 +0000

    Rework the mutators into separate classes.
    The mutators were supplied inline with the corpus mutation loop. This
    makes it tedious to extend, and difficult to filter out mutations
    which are not interesting.
    
    The code has been reworked here so that...
    
    * Each mutator is its own class.
    * Each class can provide information about what it does, such as its
      name and the types of mutations it performs.
    * Each class is registered into a list of classes that are available.
    * The Corpus instantiates these classes when it is intialised, and
      could (but does not at this time) filter the list as necessary.
      The name isn't even used yet.
    * Mutators can return None to say that they're not appropriate.
    
    This means that adding a new mutator is a matter of creating a new
    class, in the same style as the existing ones, and giving information
    on what the mutator does. Mutators could be based on one another -
    so for example the 'swap' mutator could be reworked to exchange
    variable lengths of values, rather than only bytes, and then subclassed
    to produce short, long and longlong variants. This has not been done
    here.
    
    Previously, the code attempted to retry applying mutators if they
    were not deemed appropriate; this was ineffective because they merely
    tried to decrement the iteration count, which did not affect the
    iterations at all - it looks like the code was originally using
    C-style for loops where the variable controls the termination, whilst
    in Python the range controls the iteration of this loop. This has
    been replaced by the mutator returning None to signal that it is
    inappropriate, and a loop in the caller repeats the selection of
    a new mutator.

--- pythonfuzz/corpus.py
+++ pythonfuzz/corpus.py
@@ -11,7 +11,346 @@ INTERESTING16 = [-32768, -129, 128, 255, 256, 512, 1000, 1024, 4096, 32767]
 INTERESTING32 = [-2147483648, -100663046, -32769, 32768, 65535, 65536, 100663045, 2147483647]
 
 
+# A list of all the mutator clases we have available
+mutator_classes = []
+
+
+def register_mutator(cls):
+    mutator_classes.append(cls)
+    return cls
+
+
+class Mutator(object):
+    """
+    Base class for all mutators.
+
+    All mutators are based on this class, and must provide a `mutate` method, which performs
+    some form of mutation on the input resource. Subclasses can be created which share some
+    properties with others.
+
+    Each mutator has a number of properties which can be used to select whether the mutator
+    is of interest to the user or not.
+
+        `name` - describes the mutator
+        `types` - provides a set of named types of mutations that the class performs.
+                  these types can be used to filter out uninteresting mutations.
+    """
+    name = None
+    types = set([])
+
+    def __init__(self, corpus):
+        self.corpus = corpus
+
+    @staticmethod
+    def _rand(n):
+        if n == 1 or n == 0:
+            return 0
+        return random.randint(0, n-1)
+
+    @staticmethod
+    def _choose_len(n):
+        x = Corpus._rand(100)
+        if x < 90:
+            return Corpus._rand(min(8, n)) + 1
+        elif x < 99:
+            return Corpus._rand(min(32, n)) + 1
+        else:
+            return Corpus._rand(n) + 1
+
+    @staticmethod
+    def copy(src, dst, start_source, start_dst, end_source=None, end_dst=None):
+        end_source = len(src) if end_source is None else end_source
+        end_dst = len(dst) if end_dst is None else end_dst
+        byte_to_copy = min(end_source-start_source, end_dst-start_dst)
+        src[start_source:start_source+byte_to_copy] = dst[start_dst:start_dst+byte_to_copy]
+
+    def mutate(self, res):
+        """
+        Function to mutate a given resource into another one.
+
+        @return: new resource, or None if this mutator is not appropriate.
+        """
+        raise NotImplementedError('mutate not implemented in {}'.format(self.__class__.__name__))
+
+
+@register_mutator
+class MutatorRemoveRange(Mutator):
+    name = 'Remove a range of bytes'
+    types = set(['byte'])
+
+    def mutate(self, res):
+        if len(res) < 2:
+            # Originally this checked the size of the corpus; we merely check whether the
+            # resource is long. If not, we give up.
+            return None
+
+        pos0 = self._rand(len(res))
+        pos1 = pos0 + self._choose_len(len(res) - pos0)
+        self.copy(res, res, pos1, pos0)
+        return res[:len(res) - (pos1-pos0)]
+
+
+@register_mutator
+class MutatorInsertBytes(Mutator):
+    name = 'Insert a range of random bytes'
+    types = set(['byte'])
+
+    def mutate(self, res):
+        pos = self._rand(len(res) + 1)
+        n = self._choose_len(10)
+        for k in range(n):
+            res.append(0)
+        self.copy(res, res, pos, pos+n)
+        for k in range(n):
+            res[pos+k] = self._rand(256)
+        return res
+
+
+@register_mutator
+class MutatorDuplicateBytes(Mutator):
+    name = 'Duplicate a range of bytes'
+    types = set(['byte'])
+
+    def mutate(self, res):
+        if len(res) <= 1:
+            return None
+        src = self._rand(len(res))
+        dst = self._rand(len(res))
+        while src == dst:
+            dst = self._rand(len(res))
+        n = self._choose_len(len(res) - src)
+        tmp = bytearray(n)
+        self.copy(res, tmp, src, 0)
+        for k in range(n):
+            res.append(0)
+        self.copy(res, res, dst, dst+n)
+        for k in range(n):
+            res[dst+k] = tmp[k]
+        return res
+
+
+@register_mutator
+class MutatorCopyBytes(Mutator):
+    name = 'Copy a range of bytes'
+    types = set(['byte'])
+
+    def mutate(self, res):
+        if len(res) <= 1:
+            return None
+        src = self._rand(len(res))
+        dst = self._rand(len(res))
+        while src == dst:
+            dst = self._rand(len(res))
+        n = self._choose_len(len(res) - src)
+        self.copy(res, res, src, dst, src+n)
+        return res
+
+
+@register_mutator
+class MutatorBitFlip(Mutator):
+    name = 'Bit flip'
+    types = set(['bit'])
+
+    def mutate(self, res):
+        if len(res) == 0:
+            return None
+        pos = self._rand(len(res))
+        res[pos] ^= 1 << self._rand(8)
+        return res
+
+
+@register_mutator
+class MutatorRandomiseByte(Mutator):
+    name = 'Set a byte to a random value.'
+    types = set(['byte'])
+
+    def mutate(self, res):
+        if len(res) == 0:
+            return None
+        pos = self._rand(len(res))
+        res[pos] ^= self._rand(255) + 1
+        return res
+
+
+@register_mutator
+class MutatorSwapBytes(Mutator):
+    name = 'Swap 2 bytes'
+    types = set(['byte'])
+
+    def mutate(self, res):
+        if len(res) <= 1:
+            return None
+        src = self._rand(len(res))
+        dst = self._rand(len(res))
+        while src == dst:
+            dst = self._rand(len(res))
+        res[src], res[dst] = res[dst], res[src]
+        return res
+
+
+@register_mutator
+class MutatorAddSubByte(Mutator):
+    name = 'Add/subtract from a byte'
+    types = set(['byte'])
+
+    def mutate(self, res):
+        if len(res) == 0:
+            return None
+        pos = self._rand(len(res))
+        v = self._rand(35) + 1
+        if bool(random.getrandbits(1)):
+            res[pos] = numpy.uint8(res[pos]) + numpy.uint8(v)
+        else:
+            res[pos] = numpy.uint8(res[pos]) - numpy.uint8(v)
+        return res
+
+
+@register_mutator
+class MutatorAddSubShort(Mutator):
+    name = 'Add/subtract from a uint16'
+    types = set(['short'])
+
+    def mutate(self, res):
+        if len(res) < 2:
+            return None
+        pos = self._rand(len(res) - 1)
+        v = numpy.uint16(self._rand(35) + 1)
+        if bool(random.getrandbits(1)):
+            v = numpy.uint16(0) - v
+        if bool(random.getrandbits(1)):
+            v = struct.pack('>H', v)
+        else:
+            v = struct.pack('<H', v)
+        v = bytearray(v)
+        res[pos] = numpy.uint8(res[pos] + v[0])
+        res[pos+1] = numpy.uint8(res[pos] + v[1])
+        return res
+
+
+@register_mutator
+class MutatorAddSubLong(Mutator):
+    name = 'Add/subtract from a uint32'
+    types = set(['long'])
+
+    def mutate(self, res):
+        if len(res) < 4:
+            return None
+        pos = self._rand(len(res) - 3)
+        v = numpy.uint32(self._rand(35) + 1)
+        if bool(random.getrandbits(1)):
+            v = numpy.uint32(0) - v
+        if bool(random.getrandbits(1)):
+            v = struct.pack('>I', v)
+        else:
+            v = struct.pack('<I', v)
+        v = bytearray(v)
+        for k in range(4):
+            res[pos+k] = numpy.uint8(res[pos+k] + v[k])
+        return res
+
+
+@register_mutator
+class MutatorAddSubLongLong(Mutator):
+    name = 'Add/subtract from a uint64'
+    types = set(['longlong'])
+
+    def mutate(self, res):
+        if len(res) < 8:
+            return None
+        pos = self._rand(len(res) - 7)
+        v = numpy.uint64(self._rand(35) + 1)
+        if bool(random.getrandbits(1)):
+            v = numpy.uint64(0) - v
+        if bool(random.getrandbits(1)):
+            v = struct.pack('>Q', v)
+        else:
+            v = struct.pack('<Q', v)
+        v = bytearray(v)
+        for k in range(8):
+            res[pos+k] = numpy.uint8(res[pos+k] + v[k])
+        return res
+
+
+@register_mutator
+class MutatorReplaceByte(Mutator):
+    name = 'Replace a byte with an interesting value'
+    types = set(['byte'])
+
+    def mutate(self, res):
+        if len(res) == 0:
+            return None
+        pos = self._rand(len(res))
+        res[pos] = numpy.uint8(INTERESTING8[self._rand(len(INTERESTING8))])
+        return res
+
+
+@register_mutator
+class MutatorReplaceShort(Mutator):
+    name = 'Replace an uint16 with an interesting value'
+    types = set(['short'])
+
+    def mutate(self, res):
+        if len(res) < 2:
+            return None
+        pos = self._rand(len(res) - 1)
+        v = numpy.uint16(INTERESTING16[self._rand(len(INTERESTING16))])
+        if bool(random.getrandbits(1)):
+            v = struct.pack('>H', v)
+        else:
+            v = struct.pack('<H', v)
+        v = bytearray(v)
+        res[pos] = numpy.uint8(v[0])
+        res[pos+1] = numpy.uint8(v[1])
+        return res
+
+
+@register_mutator
+class MutatorReplaceLong(Mutator):
+    name = 'Replace an uint32 with an interesting value'
+    types = set(['long'])
+
+    def mutate(self, res):
+        if len(res) < 4:
+            return None
+        pos = self._rand(len(res) - 3)
+        v = numpy.uint32(INTERESTING32[self._rand(len(INTERESTING32))])
+        if bool(random.getrandbits(1)):
+            v = struct.pack('>I', v)
+        else:
+            v = struct.pack('<I', v)
+        v = bytearray(v)
+        for k in range(4):
+            res[pos+k] = numpy.uint8(v[k])
+        return res
+
+
+@register_mutator
+class MutatorReplaceDigit(Mutator):
+    name = 'Replace an ascii digit with another digit'
+    types = set(['byte', 'ascii'])
+
+    def mutate(self, res):
+        digits = []
+        for k in range(len(res)):
+            if ord('0') <= res[k] <= ord('9'):
+                digits.append(k)
+        if len(digits) == 0:
+            return None
+        pos = self._rand(len(digits))
+        was = res[digits[pos]]
+        now = was
+        while was == now:
+            now = self._rand(10) + ord('0')
+        res[digits[pos]] = now
+        return res
+
+
+class CorpusError(Exception):
+    pass
+
+
 class Corpus(object):
+
     def __init__(self, dirs=None, max_input_size=4096):
         self._inputs = []
         self._max_input_size = max_input_size
@@ -30,6 +369,16 @@ class Corpus(object):
         self._seed_run_finished = not self._inputs
         self._seed_idx = 0
         self._save_corpus = dirs and os.path.isdir(dirs[0])
+
+        # Construct an object for each mutator we can use
+        self._mutators = [cls(self) for cls in mutator_classes]
+        if not self._mutators:
+            raise CorpusError("No mutators are available")
+
+    def __repr__(self):
+        return "<{}(corpus of {}, %i mutators)>".format(self.__class__.__name__,
+                                                        len(self._inputs),
+                                                        len(self._mutators))
 
     def _add_file(self, path):
         with open(path, 'rb') as f:
@@ -57,23 +406,6 @@ class Corpus(object):
             else:
                 break
         return count
-
-    @staticmethod
-    def _choose_len(n):
-        x = Corpus._rand(100)
-        if x < 90:
-            return Corpus._rand(min(8, n)) + 1
-        elif x < 99:
-            return Corpus._rand(min(32, n)) + 1
-        else:
-            return Corpus._rand(n) + 1
-
-    @staticmethod
-    def copy(src, dst, start_source, start_dst, end_source=None, end_dst=None):
-        end_source = len(src) if end_source is None else end_source
-        end_dst = len(dst) if end_dst is None else end_dst
-        byte_to_copy = min(end_source-start_source, end_dst-start_dst)
-        src[start_source:start_source+byte_to_copy] = dst[start_dst:start_dst+byte_to_copy]
 
     def put(self, buf):
         self._inputs.append(buf)
@@ -104,186 +436,16 @@ class Corpus(object):
         res = buf[:]
         nm = self._rand_exp()
         for i in range(nm):
-            # Remove a range of bytes.
-            x = self._rand(15)
-            if x == 0:
-                if len(self._inputs) <= 1:
-                    i -= 1
-                    continue
-                pos0 = self._rand(len(res))
-                pos1 = pos0 + self._choose_len(len(res) - pos0)
-                self.copy(res, res, pos1, pos0)
-                res = res[:len(res) - (pos1-pos0)]
-            elif x == 1:
-                # Insert a range of random bytes.
-                pos = self._rand(len(res) + 1)
-                n = self._choose_len(10)
-                for k in range(n):
-                    res.append(0)
-                self.copy(res, res, pos, pos+n)
-                for k in range(n):
-                    res[pos+k] = self._rand(256)
-            elif x == 2:
-                # Duplicate a range of bytes.
-                if len(res) <= 1:
-                    i -= 1
-                    continue
-                src = self._rand(len(res))
-                dst = self._rand(len(res))
-                while src == dst:
-                    dst = self._rand(len(res))
-                n = self._choose_len(len(res) - src)
-                tmp = bytearray(n)
-                self.copy(res, tmp, src, 0)
-                for k in range(n):
-                    res.append(0)
-                self.copy(res, res, dst, dst+n)
-                for k in range(n):
-                    res[dst+k] = tmp[k]
-            elif x == 3:
-                # Copy a range of bytes.
-                if len(res) <= 1:
-                    i -= 1
-                    continue
-                src = self._rand(len(res))
-                dst = self._rand(len(res))
-                while src == dst:
-                    dst = self._rand(len(res))
-                n = self._choose_len(len(res) - src)
-                self.copy(res, res, src, dst, src+n)
-            elif x == 4:
-                # Bit flip. Spooky!
-                if len(res) == 0:
-                    i -= 1
-                    continue
-                pos = self._rand(len(res))
-                res[pos] ^= 1 << self._rand(8)
-            elif x == 5:
-                # Set a byte to a random value.
-                if len(res) == 0:
-                    i -= 1
-                    continue
-                pos = self._rand(len(res))
-                res[pos] ^= self._rand(255) + 1
-            elif x == 6:
-                # Swap 2 bytes.
-                if len(res) <= 1:
-                    i -= 1
-                    continue
-                src = self._rand(len(res))
-                dst = self._rand(len(res))
-                while src == dst:
-                    dst = self._rand(len(res))
-                res[src], res[dst] = res[dst], res[src]
-            elif x == 7:
-                # Add/subtract from a byte.
-                if len(res) == 0:
-                    i -= 1
-                    continue
-                pos = self._rand(len(res))
-                v = self._rand(35) + 1
-                if bool(random.getrandbits(1)):
-                    res[pos] = numpy.uint8(res[pos]) + numpy.uint8(v)
-                else:
-                    res[pos] = numpy.uint8(res[pos]) - numpy.uint8(v)
-            elif x == 8:
-                # Add/subtract from a uint16.
-                if len(res) < 2:
-                    i -= 1
-                    continue
-                pos = self._rand(len(res) - 1)
-                v = numpy.uint16(self._rand(35) + 1)
-                if bool(random.getrandbits(1)):
-                    v = numpy.uint16(0) - v
-                if bool(random.getrandbits(1)):
-                    v = struct.pack('>H', v)
-                else:
-                    v = struct.pack('<H', v)
-                v = bytearray(v)
-                res[pos] = numpy.uint8(res[pos] + v[0])
-                res[pos+1] = numpy.uint8(res[pos] + v[1])
-            elif x == 9:
-                # Add/subtract from a uint32.
-                if len(res) < 4:
-                    i -= 1
-                    continue
-                pos = self._rand(len(res) - 3)
-                v = numpy.uint32(self._rand(35) + 1)
-                if bool(random.getrandbits(1)):
-                    v = numpy.uint32(0) - v
-                if bool(random.getrandbits(1)):
-                    v = struct.pack('>I', v)
-                else:
-                    v = struct.pack('<I', v)
-                v = bytearray(v)
-                for k in range(4):
-                    res[pos+k] = numpy.uint8(res[pos+k] + v[k])
-            elif x == 10:
-                # Add/subtract from a uint64.
-                if len(res) < 8:
-                    i -= 1
-                    continue
-                pos = self._rand(len(res) - 7)
-                v = numpy.uint64(self._rand(35) + 1)
-                if bool(random.getrandbits(1)):
-                    v = numpy.uint64(0) - v
-                if bool(random.getrandbits(1)):
-                    v = struct.pack('>Q', v)
-                else:
-                    v = struct.pack('<Q', v)
-                v = bytearray(v)
-                for k in range(8):
-                    res[pos+k] = numpy.uint8(res[pos+k] + v[k])
-            elif x == 11:
-                # Replace a byte with an interesting value.
-                if len(res) == 0:
-                    i -= 1
-                    continue
-                pos = self._rand(len(res))
-                res[pos] = numpy.uint8(INTERESTING8[self._rand(len(INTERESTING8))])
-            elif x == 12:
-                # Replace an uint16 with an interesting value.
-                if len(res) < 2:
-                    i -= 1
-                    continue
-                pos = self._rand(len(res) - 1)
-                v = numpy.uint16(INTERESTING16[self._rand(len(INTERESTING16))])
-                if bool(random.getrandbits(1)):
-                    v = struct.pack('>H', v)
-                else:
-                    v = struct.pack('<H', v)
-                v = bytearray(v)
-                res[pos] = numpy.uint8(v[0])
-                res[pos+1] = numpy.uint8(v[1])
-            elif x == 13:
-                # Replace an uint32 with an interesting value.
-                if len(res) < 4:
-                    i -= 1
-                    continue
-                pos = self._rand(len(res) - 3)
-                v = numpy.uint32(INTERESTING32[self._rand(len(INTERESTING32))])
-                if bool(random.getrandbits(1)):
-                    v = struct.pack('>I', v)
-                else:
-                    v = struct.pack('<I', v)
-                v = bytearray(v)
-                for k in range(4):
-                    res[pos+k] = numpy.uint8(v[k])
-            elif x == 14:
-                # Replace an ascii digit with another digit.
-                digits = []
-                for k in range(len(res)):
-                    if ord('0') <= res[k] <= ord('9'):
-                        digits.append(k)
-                if len(digits) == 0:
-                    i -= 1
-                    continue
-                pos = self._rand(len(digits))
-                was = res[digits[pos]]
-                now = was
-                while was == now:
-                    now = self._rand(10) + ord('0')
-                res[digits[pos]] = now
+
+            # Select a mutator from those we can apply
+            while True:
+                x = self._rand(len(self._mutators))
+                mutator = self._mutators[x]
+
+                newres = mutator.mutate(res)
+                if newres is not None:
+                    break
+            res = newres
 
         if len(res) > self._max_input_size:
             res = res[:self._max_input_size]

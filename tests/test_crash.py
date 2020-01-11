3d05025f6fa8220f7a917ea7528dcb38f41ddc49
Charles Ferguson <gerph@gerph.org>
2020-01-11 15:41:30 +0000

    Create a requirements-test.txt; update the tests to work on Python 2.
    The requirements-test.txt file describes what the tests need in order
    to run. That's distinct from the requirements to install - and it's
    needed because the unittest.mock interface isn't present on Python 2.7,
    so we need to use a backport.
    
    This module is then used conditionally in the two tests we currently
    have, so that we can run in either version.
    
    The test classes have also had comments added to describe what they're
    trying to test and the invariants they assume.

--- tests/test_crash.py
+++ tests/test_crash.py
@@ -1,12 +1,25 @@
 import unittest
 import zipfile
 import io
-from unittest.mock import patch
 
-import pythonfuzz
+try:
+    from unittest.mock import patch
+except ImportError:
+    # Python 2 backport of mock
+    from mock import patch
+
+import pythonfuzz.fuzzer
+
 
 class TestFindCrash(unittest.TestCase):
     def test_find_crash(self):
+        """
+        Tests that when an Exception occurs in the fuzz function, we detect this.
+
+        Requires that the Fuzzer's configuration causes it to stop when an exception
+        is detected, and that the fuzzer will generate an invalid zip file.
+        Detects the exception implicitly by the fact that a logger call was made.
+        """
         def fuzz(buf):
             f = io.BytesIO(buf)
             z = zipfile.ZipFile(f)

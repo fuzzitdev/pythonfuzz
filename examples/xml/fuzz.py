5e2a05e0ba0f3b7f4e574b09271edfc96ddd9d6b
Charles Ferguson <gerph@gerph.org>
2020-01-11 20:14:57 +0000

    Update the xml and htmlparser examples to work on Python 2.
    Some simple checks for the behaviour of the modules under Python
    2 compared to Python 3.

--- examples/xml/fuzz.py
+++ examples/xml/fuzz.py
@@ -1,12 +1,19 @@
 import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import ParseError
+import sys
+
 from pythonfuzz.main import PythonFuzz
 
 
 @PythonFuzz
 def fuzz(buf):
     try:
-        string = buf.decode("utf-8")
+        # In Python 2, the ElementTree only consumes bytes, not unicode strings,
+        # so we need to supply the correct format depending on the python version.
+        if sys.version_info[0] == 2:
+            string = bytes(buf)
+        else:
+            string = buf.decode("utf-8")
         ET.fromstring(string)
     except (UnicodeDecodeError, ParseError):
         pass
